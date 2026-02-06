ae0ed160f1c275cf356425ef0b57c66cfda20bf4
Sun Podder <contact.sunpodder09@gmail.com>
2026-02-06 18:52:00 +0600

    feat: integrate QTermWidget into TerminalPanel for enhanced terminal functionality

--- src/ui/widgets/TerminalPanel.cpp
+++ src/ui/widgets/TerminalPanel.cpp
@@ -1,4 +1,8 @@
 #include "TerminalPanel.hpp"
+#include <QVBoxLayout>
+#include <QApplication>
+#include <QClipboard>
+#include <qtermwidget.h>
 
 namespace Luna::UI {
 
@@ -7,12 +11,96 @@ TerminalPanel::TerminalPanel(QWidget *parent) : QWidget(parent) {
 }
 
 void TerminalPanel::setupUi() {
-    setStyleSheet("background-color: #1e1e1e; color: #cccccc; border-top: 1px solid #333333;");
     auto *layout = new QVBoxLayout(this);
+    layout->setContentsMargins(0, 0, 0, 0);
+
+    m_termWidget = new QTermWidget();
     
-    auto *label = new QLabel("Terminal", this);
-    label->setAlignment(Qt::AlignCenter);
-    layout->addWidget(label);
+    // Configure terminal
+    auto font = QFont("Cascadia Code", 10);
+    font.setStyleHint(QFont::Monospace);
+    m_termWidget->setTerminalFont(font);
+    
+    m_termWidget->setColorScheme("DarkPastels");
+    m_termWidget->setScrollBarPosition(QTermWidget::ScrollBarRight);
+
+    // Custom Scrollbar Style
+    m_termWidget->setStyleSheet(
+        "QScrollBar:vertical {"
+        "    border: none;"
+        "    background: #1e1e1e;"
+        "    width: 14px;"
+        "    margin: 0px 0px 0px 0px;"
+        "}"
+        "QScrollBar::handle:vertical {"
+        "    background: #424242;"
+        "    min-height: 20px;"
+        "}"
+        "QScrollBar::handle:vertical:hover {"
+        "    background: #4f4f4f;"
+        "}"
+        "QScrollBar::handle:vertical:pressed {"
+        "    background: #007fd4;"
+        "}"
+        "QScrollBar::add-line:vertical {"
+        "    height: 0px;"
+        "    subcontrol-position: bottom;"
+        "    subcontrol-origin: margin;"
+        "}"
+        "QScrollBar::sub-line:vertical {"
+        "    height: 0px;"
+        "    subcontrol-position: top;"
+        "    subcontrol-origin: margin;"
+        "}"
+        "QScrollBar::add-page:vertical, QScrollBar::sub-page:vertical {"
+        "    background: none;"
+        "}"
+        "QScrollBar:horizontal {"
+        "    border: none;"
+        "    background: #1e1e1e;"
+        "    height: 14px;"
+        "    margin: 0px 0px 0px 0px;"
+        "}"
+        "QScrollBar::handle:horizontal {"
+        "    background: #424242;"
+        "    min-width: 20px;"
+        "}"
+        "QScrollBar::handle:horizontal:hover {"
+        "    background: #4f4f4f;"
+        "}"
+        "QScrollBar::handle:horizontal:pressed {"
+        "    background: #007fd4;"
+        "}"
+        "QScrollBar::add-line:horizontal {"
+        "    width: 0px;"
+        "    subcontrol-position: right;"
+        "    subcontrol-origin: margin;"
+        "}"
+        "QScrollBar::sub-line:horizontal {"
+        "    width: 0px;"
+        "    subcontrol-position: left;"
+        "    subcontrol-origin: margin;"
+        "}"
+        "QScrollBar::add-page:horizontal, QScrollBar::sub-page:horizontal {"
+        "    background: none;"
+        "}"
+    );
+    
+    layout->addWidget(m_termWidget);
+    
+    m_termWidget->startShellProgram();
+}
+
+void TerminalPanel::sendText(const QString &text) {
+    m_termWidget->sendText(text);
+}
+
+void TerminalPanel::copyClipboard() {
+    m_termWidget->copyClipboard();
+}
+
+void TerminalPanel::pasteClipboard() {
+    m_termWidget->pasteClipboard();
 }
 
 }
--- src/ui/widgets/TerminalPanel.hpp
+++ src/ui/widgets/TerminalPanel.hpp
@@ -1,7 +1,8 @@
 #pragma once
 #include <QWidget>
-#include <QLabel>
 #include <QVBoxLayout>
+
+class QTermWidget;
 
 namespace Luna::UI {
 
@@ -10,8 +11,15 @@ class TerminalPanel : public QWidget {
 public:
     explicit TerminalPanel(QWidget *parent = nullptr);
 
+    void sendText(const QString &text);
+
+public slots:
+    void copyClipboard();
+    void pasteClipboard();
+
 private:
     void setupUi();
+    QTermWidget *m_termWidget{nullptr};
 };
 
 }
